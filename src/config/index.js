79a750cf6e4d1527676ba768081eb04aa5556eac
GCMoura <51333439+GCMoura@users.noreply.github.com>
2020-08-01 12:07:20 -0300

    url atualizada

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
-// const url = window.location.hostname.includes('localhost')
-// ? 'http://localhost:5000'
-// : 'http://learningflix.herokuapp.com'
+const USE_URL = window.location.hostname.includes('localhost')
+? 'http://localhost:5000'
+: 'https://learningflix.herokuapp.com'
 
-const USE_URL = 'http://localhost:5000'
+// const USE_URL = 'http://localhost:5000'
 
 export default USE_URL
